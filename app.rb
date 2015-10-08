d998bf3d9108bfaa6ebb1d143242d9d2c473a9c1
Tolulope <tamenze@gmail.com>
2015-10-08 11:48:50 -0400

    Basis of our music talk app

---
+++ app.rb
@@ -0,0 +1,14 @@
+require "sinatra"
+require "sinatra/activerecord"
+# require "./models"
+
+
+set :database, "sqlite3:ourvinyl.sqlite3"
+
+
+
+# get "/sign-in" do 
+# 	erb :sign_in_form
+# end
+
+# =>  include option to sign up
