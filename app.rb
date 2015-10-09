5454b9f541975103bd50c793ec58712f17085ee1
Tolulope <tamenze@gmail.com>
2015-10-09 00:12:06 -0400

    Added models page, sign-in page, and currently working on app.rbroutes and further javascript styling

--- app.rb
+++ app.rb
@@ -1,14 +1,44 @@
 require "sinatra"
 require "sinatra/activerecord"
-# require "./models"
+require "./models"
 
 
 set :database, "sqlite3:ourvinyl.sqlite3"
+enable :sessions
+
+get "/" do
+	if session[:user_id]
+		@user = User.find(session[:user_id])
+	end
+	erb :home
+end
 
 
+get "/sign-in" do 
+	erb :sign_in_form
+end
 
-# get "/sign-in" do 
-# 	erb :sign_in_form
-# end
+post "/sign-in" do
+	@user = User.where(email: params[:email]).first
+	
+	if @user && user.password == params[:password]
+		session[:user_id]=@user.id 
+		flash[:notice] = "Yuppppppp" #to be changed
+	else
+		flash[:alert] = "Nooooope" #to be changed
+	end
+
+	redirect "/"
+end
+
+post "/sign-up" do
+	@user = User.new(:email=> params[:email],:fname=> params[:fname], :lname=>params[:lname], :username=>params[:username],:password=>params[:password])
+
+		if   #doesn't already exist, add info
+		#to database
+		else 
+		flash[:alert] = "Nooooo" to 
+		end 
+end
 
 # =>  include option to sign up
